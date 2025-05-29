28929614eb31dc989aaa9f8ebf75d9b8c22a9b0e
Mohit Pandey <mohitpandey555327@gmail.com>
2025-05-29 16:08:26 +0530

    modify deploy.js

--- deploy.js
+++ deploy.js
@@ -1,11 +1,21 @@
 import ghpages from 'gh-pages';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Fix __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 ghpages.publish(
-  'dist',
+  path.join(__dirname, 'dist'), // Full path to 'dist'
   {
     branch: 'gh-pages',
     repo: 'https://github.com/Mohit3126/EVM.git',
-    message: 'Deploy from custom script (ESM)',
+    message: '🚀 Deployed using custom script',
+    user: {
+      name: 'Mohit3126',
+      email: 'you@example.com' // use your GitHub email
+    }
   },
   (err) => {
     if (err) {
